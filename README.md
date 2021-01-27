a908c029673def006f729977de6d37823e2ae2f3
Eli Bishop <eli@launchdarkly.com>
2021-01-27 11:34:15 -0800

    change CI to more of an integration test, improve docs + usability

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-### LaunchDarkly Sample Ruby Application  ###
-We've built a simple console application that demonstrates how LaunchDarkly's SDK works.  Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/).
-##### Build instructions  #####
-1. Install the LaunchDarkly Ruby SDK by running `bundle install`
-2. Copy your SDK key and feature flag key from your LaunchDarkly dashboard into `main.rb` 
-3. Run `bundle exec ruby main.rb`
+# LaunchDarkly Sample Ruby Application 
+
+We've built a simple console application that demonstrates how LaunchDarkly's SDK works.
+
+ Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/) or the [Go SDK reference guide](https://docs.launchdarkly.com/sdk/server-side/go).
+
+This demo requires Ruby version 2.5.0 or higher (or, for JRuby, 9.2.0 or higher).
+
+## Build instructions 
+
+1. Edit `main.rb` and set the value of `sdk_key` to your LaunchDarkly SDK key. If there is an existing boolean feature flag in your LaunchDarkly project that you want to evaluate, set `feature_flag_key` to the flag key.
+
+```ruby
+sdk_key = "1234567890abcdef"
+
+feature_flag_key = "my-flag"
+```
+
+2. If you have not installed the `bundler` tool, run `gem install bundler` on the command line.
+
+3. On the command line, run `bundle install`
+
+4. Run `bundle exec ruby main.rb`
--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 require 'ldclient-rb'
 
-log = ::Logger.new($stdout)
-log.level = ::Logger::DEBUG
-config = LaunchDarkly::Config.new({:logger => log})
-# TODO : Enter your LaunchDarkly SDK key here
-client = LaunchDarkly::LDClient.new("YOUR_SDK_KEY", config)
+# Set sdk_key to your LaunchDarkly SDK key before running
+sdk_key = ""
 
+# Set feature_flag_key to the feature flag key you want to evaluate
+feature_flag_key = "my-boolean-flag"
+
+
+if sdk_key == ""
+  puts "Please edit main.rb to set sdk_key to your LaunchDarkly SDK key first"
+  exit 1
+end
+
+
+# The only custom configuration we are doing here is to reduce the amount of logging.
+log = ::Logger.new($stdout)
+log.level = ::Logger::WARN
+config = LaunchDarkly::Config.new({:logger => log})
+
+client = LaunchDarkly::LDClient.new(sdk_key, config)
+
+# Set up the user properties. This user should appear on your LaunchDarkly users dashboard
+# soon after you run the demo.
 user = {
   key: "bob@example.com",
   firstName: "Bob",
@@ -15,13 +31,13 @@ user = {
   }
 }
 
-# TODO : Enter the key for your feature flag here
-if client.variation("YOUR_FEATURE_FLAG_KEY", user, false)
-  # application code to show the feature
-  puts "Showing your feature to #{user[:key]}"
-else
-  # the code to run if the feature is off
-  puts "Not showing your feature to #{user[:key]}"
-end
+show_feature = client.variation(feature_flag_key, user, false)
 
-client.flush()
+puts "Feature flag '#{feature_flag_key}' is #{show_feature} for this user"
+
+# Calling client.close() ensures that the SDK shuts down cleanly before the program exits.
+# Unless you do this, the SDK may not have a chance to deliver analytics events to LaunchDarkly,
+# so the user properties and the flag usage statistics may not appear on your dashboard. In a
+# normal long-running application, events would be delivered automatically in the background
+# and you would not need to close the client.
+client.close()
