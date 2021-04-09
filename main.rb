bcfc95b5e3a3a7943b491a1edf1e156a052cc8ad
Eli Bishop <eli@launchdarkly.com>
2021-04-09 11:27:54 -0700

    update to use current demo standards

--- main.rb
+++ main.rb
@@ -6,38 +6,39 @@ sdk_key = ""
 # Set feature_flag_key to the feature flag key you want to evaluate
 feature_flag_key = "my-boolean-flag"
 
+def show_message(s)
+	puts "*** #{s}"
+	puts
+end
 
 if sdk_key == ""
-  puts "Please edit main.rb to set sdk_key to your LaunchDarkly SDK key first"
+  show_message "Please edit main.rb to set sdk_key to your LaunchDarkly SDK key first"
   exit 1
 end
 
+client = LaunchDarkly::LDClient.new(sdk_key)
 
-# The only custom configuration we are doing here is to reduce the amount of logging.
-log = ::Logger.new($stdout)
-log.level = ::Logger::WARN
-config = LaunchDarkly::Config.new({:logger => log})
-
-client = LaunchDarkly::LDClient.new(sdk_key, config)
+if client.initialized?
+	show_message "SDK successfully initialized!"
+else
+	show_message "SDK failed to initialize"
+	exit 1
+end
 
 # Set up the user properties. This user should appear on your LaunchDarkly users dashboard
 # soon after you run the demo.
 user = {
-  key: "bob@example.com",
-  firstName: "Bob",
-  lastName: "Loblaw",
-  custom: {
-    groups: "beta_testers"
-  }
+  key: "example-user-key",
+  name: "Sandy"
 }
 
-show_feature = client.variation(feature_flag_key, user, false)
+flag_value = client.variation(feature_flag_key, user, false)
 
-puts "Feature flag '#{feature_flag_key}' is #{show_feature} for this user"
+show_message "Feature flag '#{feature_flag_key}' is #{flag_value} for this user"
 
-# Calling client.close() ensures that the SDK shuts down cleanly before the program exits.
-# Unless you do this, the SDK may not have a chance to deliver analytics events to LaunchDarkly,
-# so the user properties and the flag usage statistics may not appear on your dashboard. In a
-# normal long-running application, events would be delivered automatically in the background
-# and you would not need to close the client.
+# Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+# events to LaunchDarkly before the program exits. If analytics events are not delivered,
+# the user properties and flag usage statistics will not appear on your dashboard. In a
+# normal long-running application, the SDK would continue running and events would be
+# delivered automatically in the background.
 client.close()
