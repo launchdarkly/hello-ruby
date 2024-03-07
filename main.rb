92a96ae1df93e9acdc20255bc80e86ce0953b5e2
Matthew M. Keeler <mkeeler@launchdarkly.com>
2024-03-07 11:28:26 -0500

    ci: Migrate from circleci to GH actions (#11)

--- main.rb
+++ main.rb
@@ -1,36 +1,39 @@
 require 'ldclient-rb'
 
 # Set sdk_key to your LaunchDarkly SDK key before running
-sdk_key = ""
+sdk_key = ENV['LAUNCHDARKLY_SERVER_KEY']
 
 # Set feature_flag_key to the feature flag key you want to evaluate
-feature_flag_key = "my-boolean-flag"
+feature_flag_key = ENV['LAUNCHDARKLY_FLAG_KEY']
 
-def show_message(s)
-  puts "*** #{s}"
+def show_message(msg)
+  puts "*** #{msg}"
   puts
 end
 
-if sdk_key == ""
-  show_message "Please edit main.rb to set sdk_key to your LaunchDarkly SDK key first"
+if sdk_key == ''
+  show_message 'Please set the LAUNCHDARKLY_SERVER_KEY environment variable'
+  exit 1
+elsif feature_flag_key == ''
+  show_message 'Please set the LAUNCHDARKLY_FLAG_KEY environment variable'
   exit 1
 end
 
 client = LaunchDarkly::LDClient.new(sdk_key)
 
 if client.initialized?
-  show_message "SDK successfully initialized!"
+  show_message 'SDK successfully initialized!'
 else
-  show_message "SDK failed to initialize"
+  show_message 'SDK failed to initialize'
   exit 1
 end
 
 # Set up the context properties. This context should appear on your LaunchDarkly contexts dashboard
 # soon after you run the demo.
 context = LaunchDarkly::LDContext.create({
-                                           key: "example-user-key",
-                                           kind: "user",
-                                           name: "Sandy"
+                                           key: 'example-user-key',
+                                           kind: 'user',
+                                           name: 'Sandy'
                                          })
 
 flag_value = client.variation(feature_flag_key, context, false)
@@ -42,4 +45,4 @@ show_message "Feature flag '#{feature_flag_key}' is #{flag_value} for this conte
 # the context properties and flag usage statistics will not appear on your dashboard. In a
 # normal long-running application, the SDK would continue running and events would be
 # delivered automatically in the background.
-client.close()
+client.close
